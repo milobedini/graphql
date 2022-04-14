85fd47c876073b3ddb4a3f4c994d0e5fed2e6e39
Milo Bedini <milobedini64@gmail.com>
2022-04-14 12:16:20 +0100

    image serializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from .models import Product
+from .models import Product, ProductImage
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ("image", "alt_text")
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    product_image = ImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Product
         fields = (
@@ -12,4 +20,5 @@ class ProductSerializer(serializers.ModelSerializer):
             "description",
             "category",
             "regular_price",
+            "product_image",
         )
